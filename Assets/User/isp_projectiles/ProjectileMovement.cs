e55c1c9bddacbe7d9befea4849b2e764f8c28b6c
flyguywannadie <60896082+flyguywannadie@users.noreply.github.com>
2023-11-17 19:59:32 -0700

    integrated nic's projectile as a test

--- Assets/User/isp_projectiles/ProjectileMovement.cs
+++ Assets/User/isp_projectiles/ProjectileMovement.cs
@@ -1,18 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using static UnityEngine.RuleTile.TilingRuleOutput;
 
 
 namespace isp_projectiles
 {
     public class ProjectileMovement : MonoBehaviour
     {
-        public int  initialSpeed = 1;
-        public int  acceleration = 15;
+        public int initialSpeed = 1;
+        public int acceleration = 15;
         public ParticleSystem smokeEffect;
 
-        private float currentSpeed; 
+        private float currentSpeed;
         private bool smokeStart = false;
         void Start()
         {
@@ -21,33 +20,31 @@ namespace isp_projectiles
             Debug.Log("Current speed: " + currentSpeed);
         }
 
-    void Update()
-    {
-        currentSpeed += acceleration * Time.deltaTime;
-        transform.Translate(Vector2.up * currentSpeed * Time.deltaTime);
-        Debug.Log("Current speed: " + currentSpeed);
-
-            if(!smokeStart && currentSpeed > 0)
-            {
-                smokeEffect.Play();
-                smokeStart = true;
-
-            }
-
+        void Update()
+        {
+            currentSpeed += acceleration * Time.deltaTime;
+            transform.Translate(Vector3.right * currentSpeed * Time.deltaTime);
+            Debug.Log("Current speed: " + currentSpeed);
         }
 
         void OnCollisionEnter2D(Collision2D collision)
-    {
-        if (collision.gameObject.CompareTag("Target"))
+        {
+            if (collision.gameObject.CompareTag("bad_guy"))
             {
                 Debug.Log("Current speed: " + currentSpeed);
 
                 Debug.Log("Direct Hit!");
 
-                Destroy(gameObject);
-        }
-    }
+                if (smokeEffect != null)
+                {
+                    GameObject particles = Instantiate(smokeEffect.gameObject, transform.position, Quaternion.identity);
+                    particles.GetComponent<ParticleSystem>().Play();
+                }
 
-}
+                Destroy(gameObject);
+            }
+        }
+
+    }
 }
 
