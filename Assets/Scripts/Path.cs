be6cf53e04959c90e5acb7ccb5615aff0f3f7316
Makenzie Buchenroth <mbuchenroth@student.neumont.edu>
2024-01-24 13:41:27 -0700

    .

--- Assets/Scripts/Path.cs
+++ Assets/Scripts/Path.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
-//using UnityEditor;
+using UnityEditor;
 using UnityEngine;
 
 public class Path : MonoBehaviour
@@ -36,60 +36,60 @@ public class Path : MonoBehaviour
     }
 }
 
-//[CustomEditor(typeof(Path))]
-//public class ExampleEditor : Editor
-//{
-//    //
-//    // Custom in-scene UI for when ExampleScript
-//    // component is selected.
-//    public void OnSceneGUI()
-//    {
-//        var t = target as Path;
-//        var tr = t.transform;
-//        var color = new Color(1, 0.8f, 0.4f, 1);
-//        Handles.color = color;
-//        Handles.DrawPolyLine(t.pathway.ToArray());
-//        if (t.pathway.Count > 0)
-//        {
-//            for (int p = 0; p < t.pathway.Count; p++)
-//            {
-//                var pos = t.pathway[p];
-//                if (p == t.pathway.Count - 1)
-//                {
-//                    Handles.color = Color.green;
-//                    if (Handles.Button(pos + new Vector3(1, 1, 0), Quaternion.identity, 0.75f, 0.1f, Handles.SphereHandleCap))
-//                    {
-//                        t.pathway.Add(pos + new Vector3(1, 1, 0));
-//                        t.SetLineRenderer();
-//                    }
-//                    Handles.color = Color.red;
-//                    if (Handles.Button(pos + new Vector3(-1, 1, 0), Quaternion.identity, 0.75f, 0.1f, Handles.SphereHandleCap))
-//                    {
-//                        t.pathway.Remove(pos);
-//                        t.SetLineRenderer();
-//                    }
-//                }
-//                EditorGUI.BeginChangeCheck();
-//                Vector3 newTargetPosition = Handles.PositionHandle(pos, Quaternion.identity);
-//                if (EditorGUI.EndChangeCheck())
-//                {
-//                    Undo.RecordObject(t, "Change Look At Target Position");
-//                    newTargetPosition.x = Mathf.Round(newTargetPosition.x * t.pathPointPercision) / t.pathPointPercision;
-//                    newTargetPosition.y = Mathf.Round(newTargetPosition.y * t.pathPointPercision) / t.pathPointPercision;
-//                    t.pathway[p] = newTargetPosition;
-//                    t.SetLineRenderer();
-//                }
-//                Handles.Label(pos, "point " + p.ToString());
-//            }
-//        } 
-//        else
-//        {
-//            Handles.color = Color.blue;
-//            if (Handles.Button(t.gameObject.transform.position, Quaternion.identity, 0.75f, 0.1f, Handles.SphereHandleCap))
-//            {
-//                t.pathway.Add(t.gameObject.transform.position);
-//                t.SetLineRenderer();
-//            }
-//        }
-//    }
-//}
+[CustomEditor(typeof(Path))]
+public class ExampleEditor : Editor
+{
+    //
+    // Custom in-scene UI for when ExampleScript
+    // component is selected.
+    public void OnSceneGUI()
+    {
+        var t = target as Path;
+        var tr = t.transform;
+        var color = new Color(1, 0.8f, 0.4f, 1);
+        Handles.color = color;
+        Handles.DrawPolyLine(t.pathway.ToArray());
+        if (t.pathway.Count > 0)
+        {
+            for (int p = 0; p < t.pathway.Count; p++)
+            {
+                var pos = t.pathway[p];
+                if (p == t.pathway.Count - 1)
+                {
+                    Handles.color = Color.green;
+                    if (Handles.Button(pos + new Vector3(1, 1, 0), Quaternion.identity, 0.75f, 0.1f, Handles.SphereHandleCap))
+                    {
+                        t.pathway.Add(pos + new Vector3(1, 1, 0));
+                        t.SetLineRenderer();
+                    }
+                    Handles.color = Color.red;
+                    if (Handles.Button(pos + new Vector3(-1, 1, 0), Quaternion.identity, 0.75f, 0.1f, Handles.SphereHandleCap))
+                    {
+                        t.pathway.Remove(pos);
+                        t.SetLineRenderer();
+                    }
+                }
+                EditorGUI.BeginChangeCheck();
+                Vector3 newTargetPosition = Handles.PositionHandle(pos, Quaternion.identity);
+                if (EditorGUI.EndChangeCheck())
+                {
+                    Undo.RecordObject(t, "Change Look At Target Position");
+                    newTargetPosition.x = Mathf.Round(newTargetPosition.x * t.pathPointPercision) / t.pathPointPercision;
+                    newTargetPosition.y = Mathf.Round(newTargetPosition.y * t.pathPointPercision) / t.pathPointPercision;
+                    t.pathway[p] = newTargetPosition;
+                    t.SetLineRenderer();
+                }
+                Handles.Label(pos, "point " + p.ToString());
+            }
+        }
+        else
+        {
+            Handles.color = Color.blue;
+            if (Handles.Button(t.gameObject.transform.position, Quaternion.identity, 0.75f, 0.1f, Handles.SphereHandleCap))
+            {
+                t.pathway.Add(t.gameObject.transform.position);
+                t.SetLineRenderer();
+            }
+        }
+    }
+}
